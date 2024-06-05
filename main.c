ffa689648e021ee21dd6e4a40c52486791ea3bbb
IBruno72I <bruno72.rocha@gmail.com>
2024-06-05 16:00:31 +0200

    Fenetre et touche F11(agrandir) + Echap(quit)

--- main.c
+++ main.c
@@ -7,8 +7,6 @@
 
 int main () {
 
-    printf("START\n");
-
     SDL_Window *window = NULL;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -21,16 +19,44 @@ int main () {
     window = SDL_CreateWindow(
                 "WINDOW", 
                 SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
-                640, 480, SDL_WINDOW_SHOWN
+                960, 540, 
+                SDL_WINDOW_BORDERLESS
             );
     if (window == NULL) {
         SDL_Log("Erreur : Initialisation Window > %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
+    SDL_ShowWindow(window);
+
+    
+    SDL_Event event;
+    SDL_bool quit = SDL_FALSE;
+
+    while (!quit) {
+        SDL_WaitEvent(&event);
+
+        if (event.type == SDL_KEYDOWN) {
+            if (event.key.keysym.scancode == SDL_SCANCODE_F11) {
+                if (SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN) {
+                    SDL_SetWindowFullscreen(window, 0);
+                } else {
+                    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP);
+                }
+            }
+
+            if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+                quit = SDL_TRUE;
+        }
+                
+        if (event.type == SDL_QUIT)
+            quit = SDL_TRUE;
+    }
 
 
 
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
